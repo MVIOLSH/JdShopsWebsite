43951bd943019efe910a6adbee6df58026ab2f2a
Michal <mszymanski@mv1.co.uk>
2021-06-07 17:49:15 +0100

    07/06/21 - 2

---
+++ JdShopsWebsite/Shared/AddAddressImg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JdShopsWebsite.Shared
+{
+    public class AddAddressImg
+    {
+        public int Id { get; set; }
+        public string ShopNumber { get; set; }
+        public string FileName { get; set; }
+        public string FilePath { get; set; }
+        public string Description { get; set; }
+    }
+}
