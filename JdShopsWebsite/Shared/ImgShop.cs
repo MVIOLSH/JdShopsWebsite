a7b1f212c658dea0ac98a94cc50e3c7306449557
Michal <mszymanski@mv1.co.uk>
2021-06-10 23:27:12 +0100

    First publish v1.0

--- JdShopsWebsite/Shared/ImgShop.cs
+++ JdShopsWebsite/Shared/ImgShop.cs
@@ -12,5 +12,6 @@ namespace JdShopsWebsite.Shared
         public string FileName { get; set; }
         public string FilePath { get; set; }
         public string Description { get; set; }
+        public int AddAddressId { get; set; }
     }
 }
